2952578968cf4434a48cad9e54005b467f34d876
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-09-22 13:23:40 +0200

    Add browser tests for opening dialog and acdcShow parameter

--- test/browser/specs/acdc.js
+++ test/browser/specs/acdc.js
@@ -8,14 +8,44 @@ describe( 'AC/DC', () => {
 		acdc = await fs.readFile( 'acdc.js', { encoding: 'utf8' } );
 	} );
 
-	beforeEach( 'open blank page and inject AC/DC code', async () => {
-		await browser.url( '/wiki/Special:BlankPage?uselang=en' );
+	async function injectAcdc() {
 		await browser.waitUntil( () => browser.execute( () => window.mediaWiki !== undefined && window.jQuery !== undefined ) );
 		await browser.execute( acdc );
+	}
+
+	describe( 'default mode', () => {
+		beforeEach( 'open blank page and inject AC/DC code', async () => {
+			await browser.url( '/wiki/Special:BlankPage?uselang=en' );
+			await injectAcdc();
+		} );
+
+		it( 'defines the portlet link', async () => {
+			const portletLink = await $( '#t-acdc' );
+			assert.strictEqual( await portletLink.getText(), 'AC/DC' );
+		} );
+
+		it( 'opens the dialog when clicking the portlet link', async () => {
+			const portletLink = await $( '#t-acdc' );
+			await portletLink.click();
+			const dialog = await $( '.acdc-statementsDialog' );
+			await dialog.waitForDisplayed();
+		} );
 	} );
 
-	it( 'defines the portlet link', async () => {
-		const portletLink = await $( '#t-acdc' );
-		assert.strictEqual( await portletLink.getText(), 'AC/DC' );
+	describe( 'show-immediately mode', () => {
+		beforeEach( 'open blank page and inject AC/DC code', async () => {
+			await browser.url( '/wiki/Special:BlankPage?uselang=en&acdcShow=1' );
+			await injectAcdc();
+		} );
+
+		it( 'defines the portlet link', async () => {
+			const content = await $( '#t-acdc' );
+			assert.strictEqual( await content.getText(), 'AC/DC' );
+		} );
+
+		it( 'opens the dialog', async () => {
+			const dialog = await $( '.acdc-statementsDialog' );
+			await dialog.waitForDisplayed();
+		} );
 	} );
 } );
