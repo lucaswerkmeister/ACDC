4be71e79fb81d93e889e59269f2f0b1916270c6f
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-09-23 23:56:23 +0200

    Run browser tests against Beta Commons
    Testing against Beta Commons instead of Test Commons should hopefully
    surface problems sooner, since Beta Commons always runs latest master
    (updated every ten minutes or so, I believe) and Test Commons only
    receives updates a day before real Commons (typically).

--- wdio.conf.js
+++ wdio.conf.js
@@ -98,7 +98,7 @@ module.exports.config = {
 	// with `/`, the base url gets prepended, not including the path portion of your baseUrl.
 	// If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
 	// gets prepended directly.
-	baseUrl: 'https://test-commons.wikimedia.org',
+	baseUrl: 'https://commons.wikimedia.beta.wmflabs.org',
 	//
 	// Default timeout for all waitFor* commands.
 	waitforTimeout: 10000,
