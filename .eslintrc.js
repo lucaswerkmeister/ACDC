e7b1cdf6b213fe627a558a2af29df96884f88d6a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-04-04 15:06:53 +0200

    Remove unneeded parts of .eslintrc.js
    We no longer polyfill Array.flatMap specifically since 0099ad60b2. (And
    since eslint no longer complains about Array.flatMap but that commit
    didn’t touch the eslint config, I can only assume the config changes
    became unnecessary with the bump to es2019 in 28a3a96b40.)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const notES2017Rules = require( 'eslint-config-wikimedia/language/not-es2017' );
-
 module.exports = {
     'env': {
         'browser': true,
@@ -20,9 +18,5 @@ module.exports = {
         'arrow-parens': [ 'error', 'as-needed' ],
         'comma-dangle': [ 'error', 'always-multiline' ],
         'no-irregular-whitespace': [ 'error', { 'skipStrings': true, 'skipComments': true } ],
-        // we polyfill Array.flatMap
-        'no-extend-native': [ 'error', { 'exceptions': [ 'Array' ] } ],
-        'no-restricted-properties': notES2017Rules.rules['no-restricted-properties'].filter(
-            r => typeof r !== 'object' || r.property !== 'flatMap' ),
     }
 };
