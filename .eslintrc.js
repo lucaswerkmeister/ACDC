2d47c56ffa771e0d2c0315214ab2e77a67664baf
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-07-17 01:32:29 +0200

    Update eslint packages
    Apparently the .eslintrc.js files are now checked, so fix a bunch of
    issues in those. (Which is good to do anyways, especially the wrong
    indentation in the root file.)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
+/* eslint-env node */
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-    },
-    'extends': [
-        'wikimedia',
-        'wikimedia/jquery',
-        'wikimedia/language/es2019',
-    ],
-    'globals': {
-        'mediaWiki': 'readable',
-        'jQuery': 'readable',
-        'OO': 'readable',
-        'wikibase': 'readable',
-    },
-    'rules': {
-        'arrow-parens': [ 'error', 'as-needed' ],
-        'comma-dangle': [ 'error', 'always-multiline' ],
-        'no-irregular-whitespace': [ 'error', { 'skipStrings': true, 'skipComments': true } ],
-    }
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'wikimedia',
+		'wikimedia/jquery',
+		'wikimedia/language/es2019',
+	],
+	globals: {
+		mediaWiki: 'readable',
+		jQuery: 'readable',
+		OO: 'readable',
+		wikibase: 'readable',
+	},
+	rules: {
+		'arrow-parens': [ 'error', 'as-needed' ],
+		'comma-dangle': [ 'error', 'always-multiline' ],
+		'no-irregular-whitespace': [ 'error', { skipStrings: true, skipComments: true } ],
+	},
 };
